823af25f341f2f44b73e8ad3afbf3f0ef4d51b34
josh <joshuasuresh08@gmail.com>
2026-06-08 10:06:50 +0530

    vercel deployment fixes

--- vite.config.ts
+++ vite.config.ts
@@ -37,4 +37,10 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
+  nitro: {
+    preset: "vercel",
+    output: {
+      dir: ".vercel/output",
+    },
+  },
 });
